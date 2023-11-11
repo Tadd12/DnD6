cc89b6dd1115eb30bb180a493d87e81d254ea34f
Leni <leninovello2004gmail.com>
2023-11-11 19:52:51 +0100

    did nothing

--- DnD6/Entitys/BaseClasses/Events/health_changed_event.gd
+++ DnD6/Entitys/BaseClasses/Events/health_changed_event.gd
@@ -8,24 +8,24 @@ var cause_of_change
 
 
 func _init(old: float, new: float, amount: float, reciever, cause):
-    old_health = old
-    new_health = new
-    change_value = amount
-    entity = reciever
-    cause_of_change = cause
-    
+	old_health = old
+	new_health = new
+	change_value = amount
+	entity = reciever
+	cause_of_change = cause
+	
 func get_cause():
-    return cause_of_change
+	return cause_of_change
    
 func get_entity():
-    return entity
+	return entity
 
 func get_old_hp() -> float:
-    return old_health
-    
+	return old_health
+	
 func get_new_hp() -> float:
-    return new_health
-    
+	return new_health
+	
 func get_amount() -> float:
-    return change_value
-    
+	return change_value
+	
