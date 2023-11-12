440fee029a6864c5b22e5ee45d2a1eff3e00b6c2
Kim Nissen <kibibo60@gmail.com>
2023-11-12 02:01:17 +0100

    added Damage effect and more inventory functions to base inventory

--- DnD6/Entitys/Inventory/base_inventory.gd
+++ DnD6/Entitys/Inventory/base_inventory.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name BaseInventory
 
 var contens := {}
 @export var size := 20
@@ -7,3 +8,6 @@ func add_item(item: ItemBase):
 	if contens.has(item):
 			contens[item] += 1
 	
+func add_itemlist(item_list: Array[ItemBase]):
+	for item in item_list:
+		add_item(item)
