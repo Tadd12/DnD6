1d40d5b900ef8f4f948585ee5b5092967b353da5
FFranziskus13 <148067017+FFranziskus13@users.noreply.github.com>
2024-04-24 11:34:46 +0200

    Add Inventory and Item System to the game (first draft)
    Adds the ability to open an inventory and open a chest (or similar).
    Adds Items
    Adds the ability to move Items
    Adds the ability to right click Items to show a context menu with options
    
    ---------
    
    * working inventory and Chest as external inventory
    
    * added missing files
    
    * added files
    
    * Changed file structure
    
    * Added Documentation to the Methods
    
    * Fixed write errors
    
    * fixed spelling
    
    * Fixed naming
    
    * fixed naming
    
    * fix fabians comments
    
    * added TODOs
    
    * renamed HealthPotion.png to HealthPotionW20.png and set Project to Godot 4.2
    
    * changed according to feedback
    
    * removed player ready debug message
    
    * this -> the
    
    * changed external_inventory to externalInventory
    
    * Added item options that appear on right click on an item in an inventory
    
    Adds an inventory and Item system to the Game
    
    ---------
    
    Co-authored-by: Kim Nissen <kibibo60@gmail.com>
    Co-authored-by: ramenmitpilz <114286575+ramenmitpilz@users.noreply.github.com>
    Co-authored-by: Tadd <95188667+Tadd12@users.noreply.github.com>

---
+++ DnD6/entities/Player/scripts/player.gd
@@ -0,0 +1,49 @@
+extends CharacterBase
+
+@export var speed := 200
+@onready var interactArea := $InteractArea
+
+
+signal toggleInventory
+signal closeUi
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("inventory"):
+		toggleInventory.emit()
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		closeUi.emit()
+		
+	if Input.is_action_just_pressed("interact"):
+		interact()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var vel := Vector2.ZERO
+	if Input.is_action_pressed("up"):
+		vel.y -= 1
+	if Input.is_action_pressed("down"):
+		vel.y += 1
+	if Input.is_action_pressed("left"):
+		vel.x -= 1
+	if Input.is_action_pressed("right"):
+		vel.x += 1
+	velocity = vel.normalized() * speed
+	move_and_slide()
+	
+
+#desc Checks if there is an interactable body within the [InteractArea].
+#desc If bodys are found, the [code]playerInteract[/code] method is called on the closest one.
+func interact() -> void:
+	if interactArea.get_overlapping_bodies():
+		var closest = interactArea.get_overlapping_bodies() \
+			.map(func(body): 
+				return body.get_global_position().distance_to(get_global_position())) \
+			.min()
+		interactArea.get_overlapping_bodies()[0].playerInteract()
+
