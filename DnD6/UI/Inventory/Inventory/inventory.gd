53393f0f4399ff183f5f3a646c5d50c57c54e380
Tadd <95188667+Tadd12@users.noreply.github.com>
2024-04-24 11:25:04 +0200

    Revert "Revert "7 inventory UI" (#34)"
    This reverts commit 4d010f89b508a50077edec71eaed233f99d4b61e.

---
+++ DnD6/UI/Inventory/Inventory/inventory.gd
@@ -0,0 +1,39 @@
+extends PanelContainer
+class_name Inventory
+
+const SlotScene := preload("res://UI/Inventory/Slot/slot.tscn")
+
+@onready var itemGrid := $MarginContainer/ScrollContainer/ItemGrid
+
+
+func _ready():
+	var inventoryData := preload("res://UI/Inventory/test_inv.tres") as InventoryData
+	refreshItemGrid(inventoryData)
+	
+	
+#desc Sets the data displayed in the inventory
+func setInventoryData(inventoryData: InventoryData) -> void:
+	inventoryData.inventoryUpdated.connect(refreshItemGrid)
+	refreshItemGrid(inventoryData)
+	
+	var calc_size: int =  min((inventoryData.slotDatas.size() + 4) / 5, 412)
+	size.y = 4 + 68 * calc_size
+
+	
+#desc Clears the display of the inventory
+func clearInventoryData(inventoryData: InventoryData) -> void:
+	inventoryData.inventoryUpdated.disconnect(refreshItemGrid)
+
+	
+#desc Sets the data of the slots in the inventory
+func refreshItemGrid(inventoryData: InventoryData) -> void:
+	for child in itemGrid.get_children():
+		child.queue_free()
+
+	for slotData in inventoryData.slotDatas:
+		var slot := SlotScene.instantiate() as Slot
+		itemGrid.add_child(slot)
+		
+		slot.slotClicked.connect(inventoryData._onSlotClicked)
+		if slotData:
+			slot.setSlotData(slotData)
---
+++ DnD6/UI/Inventory/ItemOptions/item_options.gd
@@ -0,0 +1,53 @@
+extends PanelContainer
+class_name ItemOptions
+
+var infoButton : Button
+var removeButton : Button
+var removeAllButton : Button
+var inventoryData: InventoryData
+var index: int
+
+@onready var vBoxContainer := $VBoxContainer
+
+func _ready():
+	infoButton = Button.new()
+	infoButton.text = "Info"
+	infoButton.add_theme_font_size_override("button_font_size", 10)
+	vBoxContainer.add_child(infoButton)
+
+	removeButton = Button.new()
+	removeButton.text = "Remove One"
+	removeButton.pressed.connect(removeOneItem)
+	removeButton.add_theme_font_size_override("button_font_size", 10)
+	vBoxContainer.add_child(removeButton)	
+
+	removeAllButton = Button.new()
+	removeAllButton.text = "Remove All"
+	removeAllButton.pressed.connect(removeAllItems)
+	removeAllButton.add_theme_font_size_override("button_font_size", 10)
+	vBoxContainer.add_child(removeAllButton)
+
+func removeOneItem() -> void:
+	var slotData := inventoryData.slotDatas[index]
+	slotData.quantity -= 1
+	if slotData.quantity < 1:
+		inventoryData.slotDatas[index] = null
+		hide()
+	inventoryData.inventoryUpdated.emit(inventoryData)
+
+func removeAllItems() -> void:
+	inventoryData.slotDatas[index] = null
+	hide()
+	inventoryData.inventoryUpdated.emit(inventoryData)
+
+func setOptions(_inventoryData: InventoryData, _index: int) -> bool:
+	# TODO: Implement Item Data and show Options
+	inventoryData = _inventoryData
+	index = _index
+	var slotData := inventoryData.slotDatas[index]
+	if not slotData:
+		return false
+	var itemData := slotData.itemData
+	#...
+	return true
+	
