88f37a734f5b376ae99e901e7de524899804cd1b
Kim Nissen <kibibo60@gmail.com>
2024-05-18 21:46:31 +0200

    fixed missing files

---
+++ DnD6/GameMechanics/DataClasses/Dialog/dialog_piece.gd
@@ -0,0 +1,9 @@
+class_name DialogPiece
+
+var text: String
+var answers: Array[String]
+
+## A Dataclass that holds a question and an [Array] of answers
+func _init(text, answers):
+    self.text = text
+    self.answers = answers
