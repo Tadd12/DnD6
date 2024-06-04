174b243af68091c42eeca9f920786f9dc82df1fb
Emelie Schönlebe <eschonlebe@gmail.com>
2024-06-04 11:05:36 +0200

    changed to camelcase and did descriptions in english

--- DnD6/Scenes/Intro/Intro.gd
+++ DnD6/Scenes/Intro/Intro.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var animation_finished = false
+var animationFinished = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,30 +9,29 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if $MarginContainer/AnimationPlayer.is_playing() == false and animation_finished == false:
-		# Rufe die Funktion auf, wenn die Animation beendet ist
-		_on_animation_finished()
+	if $MarginContainer/AnimationPlayer.is_playing() == false and animationFinished == false:
+		onAnimationFinished()
 
 # Called when an input event is received
 func _input(event):
-	# Prüfe, ob die Enter-Taste gedrückt wurde
+	# check if enter is pressed
 	if event is InputEventKey and event.pressed:
-		if animation_finished:
+		if animationFinished:
 			get_tree().change_scene_to_file("res://Scenes/Main/main.tscn")
 			return
-		# Stoppe die Animation im AnimationPlayer
+		# stop animation
 		$MarginContainer/AnimationPlayer.seek($MarginContainer/AnimationPlayer.current_animation_length, true)
-		animation_finished = true
+		animationFinished = true
 
-	# Prüfe, ob die Maus geklickt wurde
+	# check if mouse is clicked
 	if event is InputEventMouseButton and event.pressed:
-		if animation_finished:
+		if animationFinished:
 			get_tree().change_scene_to_file("res://Scenes/Main/main.tscn")
 			return
-		# Stoppe die Animation im AnimationPlayer
+		# stop animation
 		$MarginContainer/AnimationPlayer.seek($MarginContainer/AnimationPlayer.current_animation_length, true)
-		animation_finished = true
+		animationFinished = true
 
-# Funktion, die aufgerufen wird, wenn die Animation beendet ist
-func _on_animation_finished():
-	animation_finished = true
+# function is called when animation is finished
+func onAnimationFinished():
+	animationFinished = true
